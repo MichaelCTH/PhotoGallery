bd6ee6936c88e0d056e7398fe888bbd8944527f1
Michael Chen <tonghaoch@qq.com>
2019-11-08 16:14:11 +0800

    Updates

---
+++ precache-manifest.0a705356ef5259c862473265a638e1fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aae2e2263d2c66c693a4f0c006b9c79",
+    "url": "/PhotoGallery/index.html"
+  },
+  {
+    "revision": "103df61388f52d1e544d",
+    "url": "/PhotoGallery/static/css/2.7ececfe9.chunk.css"
+  },
+  {
+    "revision": "9a5909d3a763d83f464d",
+    "url": "/PhotoGallery/static/css/main.787a620f.chunk.css"
+  },
+  {
+    "revision": "103df61388f52d1e544d",
+    "url": "/PhotoGallery/static/js/2.55a495a3.chunk.js"
+  },
+  {
+    "revision": "9a5909d3a763d83f464d",
+    "url": "/PhotoGallery/static/js/main.6ce6c3bb.chunk.js"
+  },
+  {
+    "revision": "21532f956f1e2238539f",
+    "url": "/PhotoGallery/static/js/runtime-main.b0ba809e.js"
+  }
+]);
