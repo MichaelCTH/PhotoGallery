4600afb7d9409218e034043ef2be52e63b43b3cf
Michael Chen1 <michael.chen1@thomsonreuters.com>
2021-03-11 15:16:59 +0800

    fix: dependence upgrade and issue fix

--- src/components/SnapView.jsx
+++ src/components/SnapView.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import { Row } from 'antd';
+import PropTypes from 'prop-types';
 
 export default function SnapView({ curImgIdx, images, updateImageIdx }) {
   return (
     <Row type="flex" justify="space-around" align="middle" className="snap-view">
       {images.map((element, idx) => (
-        // <Avatar
-        //   shape="square"
-        //   size={64}
-        //   src={element.urls.thumb}
-        //   key={element.id}
-        //   onClick={() => updateImageIdx(idx)}
-        // />
         <img
           className={curImgIdx === idx ? 'img-select' : 'img-unselect'}
           key={element.id}
           alt={element.alt_description}
           src={element.urls.thumb}
           onClick={() => updateImageIdx(idx)}
+          onKeyDown={() => { }}
+          role="presentation"
         />
       ))}
     </Row>
   );
 }
+
+SnapView.propTypes = {
+  curImgIdx: PropTypes.number.isRequired,
+  images: PropTypes.arrayOf(PropTypes.object).isRequired,
+  updateImageIdx: PropTypes.func.isRequired,
+};
